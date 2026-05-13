f14f37e737db13f0789bcd7bdd3da09dc08e8325
pibit pibit <tinisalemmealonso@gmail.com>
2026-05-12 21:24:32 -0300

    muestra los recursos del raspi

--- client/raspberry/controller.js
+++ client/raspberry/controller.js
@@ -1,16 +1,40 @@
 const raspButton = document.getElementById("raspButton");
+/*
+{
+    cpu: cpu.currentLoad.toFixed(1),
+    ram: { usedRAMGB, totalRAMGB },
+    temp: temp.main,
+    network: { upKBps, downKBps },
+    disks: disks,
+};
+*/
+function renderData(data) {
+  const $body = document.querySelector("body");
+
+  const $cpuEl = document.createElement("div");
+  $cpuEl.textContent = data.cpu + "%";
+  $body.appendChild($cpuEl);
+
+  const $ramEl = document.createElement("div");
+  $ramEl.textContent = data.ram.usedRAMGB + "GB";
+  $body.appendChild($ramEl);
+
+  const $tempEl = document.createElement("div");
+  $tempEl.textContent = data.temp + "°c";
+  $body.appendChild($tempEl);
+
+  const $networkEl = document.createElement("div");
+  $networkEl.textContent =
+    "subida: " + data.upKBps + "KBps " + " bajada: " + data.downKBps + "KBps";
+  $body.appendChild($networkEl);
+
+  //const disksEl = document.createElement("div");
+}
 
 raspButton.addEventListener("click", () => {
   fetch("/api/raspberry")
     .then((r) => r.json())
     .then((data) => {
-      /*
-      document.body.innerHTML += `
-        <p>CPU: ${data.cpu}%</p>
-        <p>RAM: ${data.ram}%</p>
-        <p>Temp: ${data.temp}°C</p>
-    `;
-    */
-      console.log(data);
+      renderData(data);
     });
 });

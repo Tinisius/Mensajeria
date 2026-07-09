a9375b50b8b1e90c528c5c4d59076310ab977f25
Tinisius <tinisalemmealonso@gmail.com>
2026-07-09 20:27:48 -0300

    colores en el dashboard

--- client/dashboard/controller.js
+++ client/dashboard/controller.js
@@ -40,13 +40,31 @@ const raspButton = document.getElementById("raspButton");
 function updateData(data) {
   const $cpuEl = document.getElementById("cpu-value");
   $cpuEl.textContent = data.cpu + "%";
+  $cpuRowBG = document.getElementById("cpu_row").style.backgroundColor;
+  data.cpu > 90
+    ? ($cpuRowBG = "red")
+    : data.cpu > 70
+      ? ($cpuRowBG = "yellow")
+      : ($cpuRowBG = "white");
 
   const $ramEl = document.getElementById("ram-value");
   const ramPorcent = (100 * data.ram.usedRAMGB) / data.ram.totalRAMGB;
   $ramEl.textContent = `${data.ram.usedRAMGB}GB / ${data.ram.totalRAMGB}GB / ${ramPorcent.toFixed(2)}%`;
+  $ramRowBG = document.getElementById("ram_row").style.backgroundColor;
+  ramPorcent > 90
+    ? ($ramRowBG = "red")
+    : ramPorcent > 70
+      ? ($ramRowBG = "yellow")
+      : ($ramRowBG = "white");
 
   const $tempEl = document.getElementById("temp-value");
   $tempEl.textContent = data.temp ? data.temp + "°c" : "No data";
+  $tempRowBG = document.getElementById("temp_row").style.backgroundColor;
+  data.temp > 80
+    ? ($tempRowBG = "red")
+    : data.temp > 60
+      ? ($tempRowBG = "yellow")
+      : ($tempRowBG = "white");
 
   const $netEl = document.getElementById("net-value");
   $netEl.textContent = `${data.upKBps | 0}KBps / ${data.downKBps | 0}KBps`;

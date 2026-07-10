2117b7be158971d7cfafc2f24bebe808bc9446ac
Tinisius <tinisalemmealonso@gmail.com>
2026-07-09 21:34:14 -0300

    cambio colores dashboard

--- client/dashboard/controller.js
+++ client/dashboard/controller.js
@@ -46,20 +46,20 @@ function updateData(data) {
   $cpuEl.textContent = data.cpu + "%";
   const $cpuRow = document.getElementById("cpu_row");
   $cpuRow.style.backgroundColor =
-    data.cpu > 90 ? RED : data.cpu > 70 ? YELLOW : WHITE;
+    data.cpu > 90 ? RED : data.cpu > 70 ? YELLOW : "white";
 
   const $ramEl = document.getElementById("ram-value");
   const ramPorcent = (100 * data.ram.usedRAMGB) / data.ram.totalRAMGB;
   $ramEl.textContent = `${data.ram.usedRAMGB}GB / ${data.ram.totalRAMGB}GB / ${ramPorcent.toFixed(2)}%`;
   const $ramRow = document.getElementById("ram_row");
   $ramRow.style.backgroundColor =
-    ramPorcent > 90 ? RED : ramPorcent > 70 ? YELLOW : WHITE;
+    ramPorcent > 90 ? RED : ramPorcent > 70 ? YELLOW : "white";
 
   const $tempEl = document.getElementById("temp-value");
   $tempEl.textContent = data.temp ? data.temp + "°c" : "No data";
   const $tempRow = document.getElementById("temp_row");
   $tempRow.style.backgroundColor =
-    data.temp > 80 ? RED : data.temp > 60 ? YELLOW : WHITE;
+    data.temp > 80 ? RED : data.temp > 60 ? YELLOW : "white";
 
   const $netEl = document.getElementById("net-value");
   $netEl.textContent = `${data.upKBps | 0}KBps / ${data.downKBps | 0}KBps`;

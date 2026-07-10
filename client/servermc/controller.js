795e283a93d44d90828edd8543dc712065a4f31a
Tinisius <tinisalemmealonso@gmail.com>
2026-07-10 01:18:44 -0300

    implementacion de logs v2

--- client/servermc/controller.js
+++ client/servermc/controller.js
@@ -162,8 +162,8 @@ async function init() {
   if (data.ok === false) {
     showAlert("error al obtener infomacion");
   } else {
-    console.log("svdata: " + data.sv_data);
-    console.log("logs: " + data.sv_data.logs);
+    console.log("svdata: " + (await data.sv_data.json()));
+    console.log("logs: " + data.sv_data.players);
     renderLogs(data.sv_data.logs);
     changeData(data.sv_data);
     changeState(data.sv_data.state);
